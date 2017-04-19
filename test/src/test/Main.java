61f15b59c07d3087abd9490408018be2285ce8b0
Tirocinio <Tirocinio@HP-DeskTopo>
2017-04-19 16:35:30 +0200

    First Commit

---
+++ test/src/test/Main.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Main {
+	
+	static void method(int i){
+		System.out.println("Method" + i);
+	}
+	
+	public static void main(String args[]){
+		
+		int i=0;
+		
+		System.out.println(i);
+		method(i++); 
+		System.out.println(i);
+		method(++i); 
+		System.out.println(i);
+	}
+	
+}
